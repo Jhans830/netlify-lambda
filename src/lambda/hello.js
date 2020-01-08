eea835925d58d00d003e2d8e032ddc92284d20d0
EC2 Default User <jhans830@gmail.com>
2020-01-08 07:15:27 +0000

    update token

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -8,23 +8,34 @@ exports.handler = (event, context, callback) => {
   const accessToken = `amhhbnM4MzA6TmV2b2x1czIk`;
   const query = `
   query {
-    repositoryOwner(login:"jhans830"){
-      pinnedRepositories(first:10) {
-        nodes {
-          name
-          url
-          homepageUrl
-          description
-          repositoryTopics(first:10) {
-            nodes {
-              topic {
-                name
+    {
+  repository(name: "netlify-lambda", owner: "jhans830") {
+    ref(qualifiedName: "master") {
+      target {
+        ... on Commit {
+          id
+          history(first: 5) {
+            pageInfo {
+              hasNextPage
+            }
+            edges {
+              node {
+                messageHeadline
+                oid
+                message
+                author {
+                  name
+                  email
+                  date
+                }
               }
             }
           }
         }
       }
     }
+  }
+}
   }`;
 
   // Send json response to the react client app
