7520f70d55a390c7486096d29142a9cb83e92e5d
EC2 Default User <jhans830@gmail.com>
2020-01-08 06:04:07 +0000

    commit

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -4,24 +4,49 @@ const axios = require("axios");
 // intermediate between client and github apiv4
 
 exports.handler = (event, context, callback) => {
+  const URL = `https://api.github.com/graphql`;
+  const accessToken = "7e27c42644a636cbe38e5e8aa486ef1b907390ae";
+  const query = `
+  query {
+    repositoryOwner(login:"jhans830@gmail.com"){
+      pinnedRepositories(first:10) {
+        nodes {
+          name
+          url
+          homepageUrl
+          description
+          repositoryTopics(first:10) {
+            nodes {
+              topic {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }`;
+
+  // Send json response to the react client app
+  const send = body => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(body)
+    });
+  };
+
+  // Perform API call
   const getrepos = () => {
     axios({
-    method: "get",
-    url: `https://api.github.com/repos/Jhans830/netlify-lambda`,
-    headers: {
-        Authorization: `Bearer 7e27c42644a636cbe38e5e8aa486ef1b907390ae`,
-        "Content-Type": "application/json"
-    },
+      method: "POST",
+      url: URL,
+      data: JSON.stringify({ query }),
+      headers: {
+        Authorization: `Bearer 7e27c42644a636cbe38e5e8aa486ef1b907390ae`
+      }
     })
-    .then(res => {
-        callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(res.data)
-        });
-    })
-    .catch(err => {
-        callback(err);
-    });
+      .then(res => send(res.data.data.repositoryOwner.pinnedRepositories.nodes))
+      .catch(err => send(err));
   };
 
   // Make sure method is GET
